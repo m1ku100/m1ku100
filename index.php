a36a073cbc818fbdb03f1f678a5671d336248fa1
m1ku100 <ilham.puji100@gmail.com>
2016-12-02 15:43:30 -0800

    v 0.5

--- index.php
+++ index.php
@@ -109,38 +109,38 @@
                     <div class="carousel-inner">
                         <div class="item active">
                             <div class="col-sm-6">
-                                <h1><span>E</span>-SHOPPER</h1>
+                                <h1><span>B</span>aby-<span>B</span>oo</h1>
                                 <h2>Free E-Commerce Template</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                                 <button type="button" class="btn btn-default get">Get it now</button>
                             </div>
                             <div class="col-sm-6">
-                                <img src="images/home/girl1.jpg" class="girl img-responsive" alt="" />
+                                <img src="images/cara/baby_crawling-2560x1600.jpg" class="girl img-responsive" alt="" />
                                 <img src="images/home/pricing.png"  class="pricing" alt="" />
                             </div>
                         </div>
                         <div class="item">
                             <div class="col-sm-6">
-                                <h1><span>E</span>-SHOPPER</h1>
+                                <h1><span>B</span>aby-<span>B</span>oo</h1>
                                 <h2>100% Responsive Design</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                                 <button type="button" class="btn btn-default get">Get it now</button>
                             </div>
                             <div class="col-sm-6">
-                                <img src="images/home/girl2.jpg" class="girl img-responsive" alt="" />
+                                <img src="images/cara/FPO-HERO-BABY.png" class="girl img-responsive" alt="" />
                                 <img src="images/home/pricing.png"  class="pricing" alt="" />
                             </div>
                         </div>
 
                         <div class="item">
                             <div class="col-sm-6">
-                                <h1><span>E</span>-SHOPPER</h1>
+                                <h1><span>B</span>aby-<span>B</span>oo</h1>
                                 <h2>Free Ecommerce Template</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                                 <button type="button" class="btn btn-default get">Get it now</button>
                             </div>
                             <div class="col-sm-6">
-                                <img src="images/home/girl3.jpg" class="girl img-responsive" alt="" />
+                                <img src="images/cara/baby-png-12.png" class="girl img-responsive" alt="" style="width: 70%"/>
                                 <img src="images/home/pricing.png" class="pricing" alt="" />
                             </div>
                         </div>
