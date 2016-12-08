9cc0c39b577dc57b444270da59de2c387a267249
m1ku100 <ilham.puji100@gmail.com>
2016-12-08 00:18:03 -0800

    v 0.6

--- sk.php
+++ sk.php
@@ -6,14 +6,6 @@
  * Time: 4:40 AM
  */
 ?>
-<?php
-/**
- * Created by PhpStorm.
- * User: X451MA
- * Date: 11/29/2016
- * Time: 3:14 AM
- */
-?>
 
 
 <!DOCTYPE html>
@@ -75,7 +67,8 @@
             <div class="row">
                 <div class="col-sm-4">
                     <div class="logo pull-left">
-                        <a href="index.php"><img src="images/bboo.png" alt="bboo.png" style="width: 160px; height: 100px"/></a>
+                        <a href="index.php"><img src="images/bboo.png" alt="bboo.png"
+                                                 style="width: 160px; height: 100px"/></a>
                     </div>
 
                 </div>
@@ -85,14 +78,14 @@
                             <li><a href="#"><i class="fa fa-user"></i> Blog</a></li>
                             <li><a href="tentang.php"><i class="fa fa-users active"></i> Tentang</a></li>
                             <li><a href="cara.php"><i class="fa fa-crosshairs active"></i> Cara Sewa</a></li>
-                            <li><a href="sk.php"><i class="fa fa-shopping-cart active" ></i> Syarat & Ketentuan</a></li>
+                            <li><a href="sk.php"><i class="fa fa-shopping-cart active"></i> Syarat & Ketentuan</a></li>
                             <li><a href=""><i class="fa fa-list"></i> Kategori</a></li>
-                            <li><a href="login.php"><i class="fa fa-sign-in "></i>Login </a>  </li>
+                            <li><a href="login.php"><i class="fa fa-sign-in "></i>Login </a></li>
                         </ul>
                         <br>
                         <br>
 
-                        <form action="index.php" method="post" >
+                        <form action="index.php" method="post">
 
                             <div class="search_box pull-right">
                                 <input type="text" placeholder="Search"/>
@@ -109,7 +102,6 @@
 </header><!--/header-->
 
 
-
 <section>
     <br>
     <div class="container">
@@ -119,14 +111,142 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12">
-                        <div class="col-sm-6">
-                            <h2 class="title text-center">Penyewaan</h2>
+                            <div class="col-sm-6">
+                                <h2 class="title text-center">Penyewaan</h2>
+                                <h4>Pemesanan</h4>
+                                <p>Segala pemesanan dapat dilakukan melalui website <a href="index.php">Baby-Boo</a></p>
+                                <br>
+                                <h4>Biaya Delivery</h4>
+                                <p style="text-justify: inter-word">Biaya pengantaran dan pengambilan yang tertera di
+                                    halaman cart berlaku untuk pemesanan dengan tanggal mulai sewa dan tanggal
+                                    sewa yang sama. Jika dalam satu pemesanan terdapat beberapa barang dengan tanggal
+                                    mulai sewa yang berbeda, pada email konfirmasi
+                                    akan kami tambahkan biaya pengantaran sesuai jumlah pengantaran yang perlu
+                                    dilakukan.</p><br>
+                                <h4>Ketentuan Pembayaran</h4>
+                                <ol>
+                                    <li>Pembayaran wajib dilakukan dalam waktu 24 jam setelah diterimanya email
+                                        konfirmasi dan wajib memberikan bukti pembayaran.
+                                        Jika dalam 24 jam bukti pembayaran belum kami terima, maka pemesanan akan batal
+                                        secara otomatis.
+                                    </li>
+                                    <br>
+                                    <li>Jika pembayaran baru dilakukan setelah batalnya pemesanan secara otomatis, maka
+                                        kami akan mengirimkan kembali
+                                        uang Anda dalam jangka waktu maksimal 5 hari kerja.
+                                    </li>
+                                </ol>
+                                <br>
+                                <h4>Pengantaran</h4>
+                                <ol>
+                                    <li>Biaya sewa belum termasuk biaya pengantaran.</li>
+                                    <br>
+                                    <li>Kami akan mengantarkan barang dengan menggunakan kurir atau jasa pengiriman
+                                        lainnya.
+                                    </li>
+                                    <br>
+                                    <li>Biaya pengantaran tergantung dari ukuran barang dan wilayah pengiriman.</li>
+                                    <br>
+                                </ol>
+                                <h4>Perpanjangan Sewa</h4>
+                                <p>Tergantung kepada ketersediaan barang, Anda dapat memperpanjang masa sewa.
+                                    Perpanjangan masa sewa harus diberitahukan kepada kami paling lambat 10 hari sebelum
+                                    akhir masa sewa.</p><br>
+                                <h4>Pilihan Warna</h4>
+                                <p>Kecuali terdapat opsi untuk memilih warna pada website, warna dari barang yang
+                                    diantar
+                                    dapat berbeda dari warna yang ditampilkan di website kami.
+                                    Namun, kami akan memastikan tipe dan kualitas barang tersebut sama.</p><br>
+                                <h4>Keamanan</h4>
+                                <ol>
+                                    <li>Kami akan memeriksa kondisi setiap barang sebelum dan sesudah masa sewa untuk
+                                        memastikan keamanannya.
+                                    </li>
+                                    <br>
+                                    <li>Kami tidak bertanggung jawab atas masalah keamanan yang muncul setelah
+                                        pengiriman barang yang disebabkan
+                                        dari kesalahan pemakaian atau pemasangan oleh pelanggan.
+                                    </li>
+                                </ol>
+                                <br>
+                                <h4>Kebersihan</h4>
+                                <p>Kami memahami bahwa kebersihan sangatlah penting. Kami menjamin bahwa seluruh barang
+                                    yang kami kirim telah dibersihkan
+                                    dengan sebaik mungkin menggunakan bahan pembersih untuk produk bayi yang ramah
+                                    lingkungan.</p><br>
+                                <h4>Pengembalian Uang</h4>
+                                <ol>
+                                    <li>Jika barang sampai dalam kondisi yang tidak dapat diterima, silahkan langsung
+                                        hubungi petugas kami dan kami akan langsung mengembalikan uang Anda.
+                                    </li>
+                                    <br>
+                                    <li>Jika anak Anda ternyata tidak muat atau suka memakai barang yang disewa, kami
+                                        dapat mengganti pesanan Anda dengan barang lain atau memberikan
+                                        refund dalam bentuk voucher Babyloania sejumlah uang yang telah Anda bayarkan,
+                                        yang bisa Anda pakai di kemudian hari.
+                                    </li>
+                                    <br>
+                                    <li>Penggantian pesanan dan refund tidak berlaku untuk penyewaan pakaian, berupa
+                                        dress dan baju musim dingin.
+                                    </li>
+                                    <br>
+                                    <li>Jika Anda mengalami hal yang tidak diinginkan selama masa kehamilan atau saat
+                                        melahirkan,
+                                        Babyloania akan mengembalikan sepenuhnya biaya sewa yang telah Anda bayarkan.
+                                    </li>
+                                    <br>
+                                    <li>Pengembalian uang sehubungan dengan keterlambatan pembayaran, lihat di Ketentuan
+                                        Pembayaran di atas.
+                                    </li>
+                                    <br>
+                                </ol>
 
-                        </div>
+                            </div>
 
-                        <div class="col-sm-6">
-                            <h2 class="title text-center">pengembalian</h2>
-                        </div>
+                            <div class="col-sm-6">
+                                <h2 class="title text-center">pengembalian</h2>
+                                <h4>Pengembalian dan Jasa Pengambilan</h4>
+                                <ol>
+                                    <li>Di akhir masa sewa, kami akan melakukan pengambilan barang.</li>
+                                    <br>
+                                    <li>Seluruh barang yang disewa wajib dikembalikan pada tanggal yang tercantum pada
+                                        email konfirmasi.
+                                    </li>
+                                    <br>
+                                    <li>Apabila barang yang disewa gagal diambil pada waktu penjemputan, maka customer
+                                        akan dikenakan biaya penjemputan kembali sebesar Rp 50.000.
+                                    </li>
+
+                                    <br>
+                                </ol>
+                                <h4>Keterlambatan Pengembalian</h4>
+                                <p>Kami akan mengenakan biaya sewa tambahan secara proporsional untuk setiap hari
+                                    keterlambatan.</p><br>
+                                <h4>Barang Tertinggal dan Barang Terbawa</h4>
+                                <ol>
+                                    <li>Apabila barang customer terbawa oleh Babyloania pada saat penjemputan barang,
+                                        customer wajib mengambil kembali barangnya dalam waktu 2 hari setelah notifikasi
+                                        dikirimkan.
+                                        Jika dalam 2 hari barang tidak di ambil, maka barang tersebut sudah di luar
+                                        tanggung jawab Babyloania.
+                                    </li>
+                                    <br>
+                                    <li> Apabila ada barang yang tertinggal di customer pada saat penjemputan barang,
+                                        maka customer akan dikenakan biaya penjemputan kembali sebesar Rp 50.000.
+                                    </li>
+                                    <br>
+                                </ol><br>
+                                <h4>Kerusakan</h4>
+                                <ol>
+                                    <li>Kami akan memeriksa setiap barang setelah dikembalikan.</li><br>
+                                    <li>Kami tidak akan mengenakan biaya untuk noda atau kerusakan kecil.</li><br>
+                                    <li>Namun, kami akan mengenakan biaya ganti rugi atas kerusakan jika diperlukan.</li><br>
+                                    <li>Jumlah biaya ganti rugi atas kerusakan tergantung dari tingkat kerusakan.</li><br>
+                                    <li>Kami berhak mengenakan biaya ganti rugi sampai dengan 1.5x harga retail jika terjadi kerusakan berat.</li><br>
+                                    <li>Biaya ganti rugi wajib dibayarkan paling lambat 24 jam setelah tagihan kami kirim.</li><br>
+                                </ol>
+
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -135,8 +255,6 @@
     </div>
 
 </section><!--/form-->
-
-
 
 
 <footer id="footer"><!--Footer-->
@@ -240,7 +358,6 @@
     </div>
 
 </footer><!--/Footer-->
-
 
 
 <script src="js/jquery.js"></script>
