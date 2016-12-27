5b5071b5a870acd7ef7db6e157f3e195da2020b9
Ilham <ilham.puji100@gmail.com>
2016-12-28 06:40:50 +0700

    v 0.8

--- kategori.php
+++ kategori.php
@@ -26,11 +26,11 @@ session_start();
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">  
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
 </head>
 <body>
-<?php 
-include "header.php"?>
+<?php
+include "header.php" ?>
 <br>
 <div class="container">
     <div class="col-lg-1">
@@ -46,15 +46,53 @@ include "header.php"?>
     <div class="col-lg-10">
         <div class="row">
             <div class="row">
-                    <h2 class="title text-center">OUR ITEM</h2>
-                <div class="col-sm-6 col-lg-offset-3">
+                <h2 class="title text-center">OUR ITEM</h2>
 
+                <div class="col-sm-12 padding-right">
+                    <div class="features_items"><!--features_items-->
+                        <?php
+                        $query = "select * from barang";
+                        $result = mysqli_query($conn, $query);
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                ?>
+                                <div class="col-sm-4">
+                                    <div class="product-image-wrapper">
+                                        <div class="single-products">
+                                            <div class="productinfo text-center">
+                                                <img src="img/<?php echo $row['gambar'] ?>" alt=""
+                                                     style="width: 248px;height:229px "/>
+                                                <h2>Rp.<?php echo $row['harga'] ?></h2>
+                                                <p><?php echo $row['n_barang'] ?></p>
+                                                <a href="#" class="btn btn-default add-to-cart"><i
+                                                        class="fa fa-shopping-cart"></i>Add
+                                                    to cart</a>
+                                            </div>
+                                            <div class="product-overlay">
+                                                <div class="overlay-content">
+                                                    <h2>Rp.<?php echo $row['harga'] ?></h2>
+                                                    <p><?php echo $row['nama_barang'] ?></p>
+                                                    <a href="#" class="btn btn-default add-to-cart"><i
+                                                            class="fa fa-shopping-cart"></i>Add to cart</a>
+                                                </div>
+                                                <img src="images/home/new.png" class="new" alt=""/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <?php
+                            }
+                        }
+
+                        ?>
+                    </div>
                 </div>
             </div>
-
         </div>
 
     </div>
+
 
 </div>
 
